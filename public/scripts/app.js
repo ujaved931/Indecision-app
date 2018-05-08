90bcf211979b6c00b39a97e03692ab9d96122c53
Usman Javed <ujaved931@gmail.com>
2018-05-08 05:27:04 +0500

    Started working on React-app called Indecision
    Learned about WebServer
    Learned about Babel
    Learned about JSX
    Learned about JSX expressions
    Learned about Conditional Rendering

---
+++ public/scripts/app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+console.log('App.js is running!');
+
+// if statements
+// ternary operators
+// logical and operators
+
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var user = {
+  name: 'Andrew',
+  age: 26,
+  location: 'Peshawar'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
+// open two commandline prompts in root folder
+// babel src/app.js --out-file=public/scripts/app.js --presets env,react --watch
+// live-server public
---
+++ src/app.js
@@ -0,0 +1,56 @@
+console.log('App.js is running!');
+
+// if statements
+// ternary operators
+// logical and operators
+
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Andrew',
+  age: 26,
+  location: 'Peshawar'
+};
+
+
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+
+
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
+// open two commandline prompts in root folder
+// babel src/app.js --out-file=public/scripts/app.js --presets env,react --watch
+// live-server public
